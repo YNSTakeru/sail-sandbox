3bb520eed5f69bde8b4ce82fbd52666108bc7da6
YNSTakeru <95395710+YNSTakeru@users.noreply.github.com>
2024-04-03 22:28:36 +0900

    create-edit-article.phpを表示

--- routes/web.php
+++ routes/web.php
@@ -5,3 +5,7 @@ use Illuminate\Support\Facades\Route;
 Route::get('/', function () {
     return view('home');
 });
+
+Route::get("/create/edit-article", function() {
+    return view("create-edit-article");
+});
