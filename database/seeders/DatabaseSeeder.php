a0a53537d789982e4fe58f118011e63bb2f07aa1
YNSTakeru <95395710+YNSTakeru@users.noreply.github.com>
2024-04-04 14:17:07 +0900

    pagination

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -14,8 +14,8 @@ class DatabaseSeeder extends Seeder
     public function run(): void
     {
         \App\Models\User::factory(10)->create();
-        \App\Models\Tag::factory(20)->create();
-        \App\Models\Article::factory(100)->create();
+        \App\Models\Tag::factory(40)->create();
+        \App\Models\Article::factory(260)->create();
 
         $this->call([
             ArticleTagSeeder::class,
--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -97,14 +97,15 @@
                             </div>
                         @endforeach
 
-                        <ul class="pagination">
+                        {{ $articles->links() }}
+                        {{-- <ul class="pagination">
                             <li class="page-item active">
                                 <a class="page-link" href="">1</a>
                             </li>
                             <li class="page-item">
                                 <a class="page-link" href="">2</a>
                             </li>
-                        </ul>
+                        </ul> --}}
                     </div>
 
                     <div class="col-md-3">
