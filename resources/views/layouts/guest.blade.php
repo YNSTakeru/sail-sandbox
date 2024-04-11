d1249220ccb4daccd93de9d7a2cf04b84b5dd619
YNSTakeru <95395710+YNSTakeru@users.noreply.github.com>
2024-04-11 16:49:04 +0900

    ユーザーのログイン状態に応じてヘッダーを切り替える

--- resources/views/layouts/guest.blade.php
+++ resources/views/layouts/guest.blade.php
@@ -24,12 +24,27 @@
                         <!-- Add "active" class when you're on that page" -->
                         <a class="nav-link active" href="/">Home</a>
                     </li>
-                    <li class="nav-item">
-                        <a class="nav-link" href="/login">Sign in</a>
-                    </li>
-                    <li class="nav-item">
-                        <a class="nav-link" href="/register">Sign up</a>
-                    </li>
+                    @if (Auth::check())
+                        <li class="nav-item">
+                            <a class="nav-link" href="/editor"> <i class="ion-compose"></i>&nbsp;New Article </a>
+                        </li>
+                        <li class="nav-item">
+                            <a class="nav-link" href="/settings"> <i class="ion-gear-a"></i>&nbsp;Settings </a>
+                        </li>
+                        <li class="nav-item">
+                            <a class="nav-link" href="/profile/eric-simons">
+                                <img src="" class="user-pic" />
+                                {{ Auth::user()->name }}
+                            </a>
+                        </li>
+                    @else
+                        <li class="nav-item">
+                            <a class="nav-link" href="/login">Sign in</a>
+                        </li>
+                        <li class="nav-item">
+                            <a class="nav-link" href="/register">Sign up</a>
+                        </li>
+                    @endif
                 </ul>
             </div>
         </nav>
